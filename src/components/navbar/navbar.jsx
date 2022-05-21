ade2bdaa17ba1ae718104331a9ae3c8b3705d81b
venkatesh@git <vpgunake_b18@me.vjti.ac.in>
2022-05-21 19:45:07 +0530

    made navbar responsive

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,15 +1,34 @@
 import './navbar.css';
 import logo from '../../images/finvestera.jpg'
+import { FaBars} from 'react-icons/fa';
+import { useState, useRef } from 'react';
+
 function Navbar() {
+
+    const [visible, setVisible] = useState(true)
+    const faBars = useRef(null)
+
+    const handleClick = () => {
+        setVisible(!visible)
+        console.log(faBars.current.children.length)
+    }
+
+
+    
+
     return (  
         <div className="navbar">
-            <div className="logo">
+            <div className="logo" >
                 <img src={logo} />
+                <div ref={faBars}>
+                    <FaBars  className='fa-bar'  onClick={handleClick}/>
+                </div>
+               
             </div>
 
             
 
-            <ul className='nav-menu'>
+            <ul className={`nav-menu ${(visible && faBars.current.children.length===1) ? 'visible': 'hidden'}`}>
                 <li>Investment</li>
                 <li>Insurce</li>
                 <li>Finances</li>
